d32395e5069a94470ae530b5f892c51fd5aaef11
gtsenacti <gt.senacti@gmail.com>
2026-04-05 22:51:06 -0300

    GESTAOFORNECEDORES: CRUD | Arquivos iniciais.

---
+++ appGestaoFornecedores/frmCadastroFornecedores.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace appGestaoFornecedores
+{
+    partial class frmCadastroFornecedores
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmCadastroFornecedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 678);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frmCadastroFornecedores";
+            this.Text = "Aplicativo Cadastro de Fornecedores:";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
